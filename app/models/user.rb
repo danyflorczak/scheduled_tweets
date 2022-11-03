bc7356d0d3fff95ae4966004f7b65faff20fc835
daniel florczak <danyflorczak@gmail.com>
2022-11-03 12:04:49 +0100

    add: email validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  has_sercure_password
+  has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Must be valid email address" }
 end
